ebed3e19d83c7382db57f9867cf55202545a0a65
Shyam Sarraf <ramsarraf00@gmail.com>
2023-01-22 19:53:44 +0530

    deployment

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,134 +1,28 @@
-let navbar = document.querySelector('.navbar')
+let cartItem = JSON.parse(localStorage.getItem("cartProd")) || [];
 
-document.querySelector('#menu-bar').onclick = () =>{
-    navbar.classList.toggle('active');
-}
+let cart_btn = document.querySelectorAll(".cart-btn");
 
-document.querySelector('#close').onclick = () =>{
-    navbar.classList.remove('active');
-}
-
-window.onscroll = () =>{
-
-    navbar.classList.remove('active');
-
-    if(window.scrollY > 100){
-        document.querySelector('header').classList.add('active');
-    }else{
-        document.querySelector('header').classList.remove('active');
-    }
-
-}
-
-let themeToggler = document.querySelector('#theme-toggler');
-
-themeToggler.onclick = () =>{
-    themeToggler.classList.toggle('fa-sun');
-    if(themeToggler.classList.contains('fa-sun')){
-        document.querySelector('body').classList.add('active');
-    }else{
-        document.querySelector('body').classList.remove('active');
-    }
-}
-
-document.querySelectorAll('.small-image-1').forEach(images =>{
-    images.onclick = () =>{
-        document.querySelector('.big-image-1').src = images.getAttribute('src');
-    }
-});
-
-document.querySelectorAll('.small-image-2').forEach(images =>{
-    images.onclick = () =>{
-        document.querySelector('.big-image-2').src = images.getAttribute('src');
-    }
-});
-
-document.querySelectorAll('.small-image-3').forEach(images =>{
-    images.onclick = () =>{
-        document.querySelector('.big-image-3').src = images.getAttribute('src');
-    }
-});
-
-let countDate = new Date('aug 1, 2021 00:00:00').getTime();
-
-function countDown(){
-
-    let now = new Date().getTime();
-	gap = countDate - now;
-
-    let seconds = 1000;
-    let minutes = seconds * 60;
-    let hours = minutes * 60;
-    let days = hours * 24;
-
-    let d = Math.floor(gap / (days));
-	let h = Math.floor((gap % (days)) / (hours));
-	let m = Math.floor((gap % (hours)) / (minutes));
-	let s = Math.floor((gap % (minutes)) / (seconds));
-
-    document.getElementById('days').innerText = d;
-    document.getElementById('hours').innerText = h;
-    document.getElementById('minutes').innerText = m;
-    document.getElementById('seconds').innerText = s;
-
-}
-
-setInterval(function(){
-    countDown()
-},1000);
-
-var swiper = new Swiper(".product-slider", {
-    slidesPerView: 3,
-    loop:true,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        550: {
-          slidesPerView: 2,
-        },
-        800: {
-          slidesPerView: 3,
-        },
-        1000: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".review-slider", {
-    slidesPerView: 3,
-    loop:true,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        550: {
-          slidesPerView: 2,
-        },
-        800: {
-          slidesPerView: 3,
-        },
-        1000: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-
-let cart_btn = document.querySelectorAll("cart-btn");
-cart_btn.addEventListner()
+Array.from(cart_btn).map((elem) => {
+    elem.addEventListener('click', (e) => {
+        let img = e.target.parentElement.parentElement.children[0].src;
+        let prodName = e.target.parentElement.parentElement.parentElement.children[2].children[0].innerText;
+        let price = e.target.parentElement.parentElement.parentElement.children[2].children[1].innerText.split(" ")[0];
+        price = price.substring(1);
+        let id = e.target.parentElement.parentElement.parentElement.id;
+        let obj = { img, prodName, price, id, quant: 1 };
+        let isPresent = false;
+        if (localStorage.getItem('cartProd') != null) {
+            cartItem.map((item) => {
+                if (item.id == id) {
+                    item.quant++;
+                    localStorage.setItem('cartProd', JSON.stringify(cartItem));
+                    isPresent = true;
+                }
+            })
+        }
+        if (!isPresent) {
+            cartItem.push(obj);
+            localStorage.setItem('cartProd', JSON.stringify(cartItem));
+        }
+    })
+})
